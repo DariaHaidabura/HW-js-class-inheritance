6d5350b0e51db521872500e960f2a531c7fb6071
Dasha <dariagaidabura@gmail.com>
2021-12-22 13:34:40 +0200

    fixed

--- index.js
+++ index.js
@@ -1,55 +1,43 @@
 let createDate = document.querySelector('p');
-let isTrue = true;
 
 function Clock(elem) {
   this.createDate = elem;
-};
-
-Clock.prototype.showDate = function () {
-	let dateFormat = new Date();
-	this.dateArea.innerHTML = `<p>Current date: ${dateFormat}</p>`;
+  this.fullFormat = false;
+  this.hours;
+  this.min;
+  this.sec;
 }
 
-function Full(elem){
+function ClockWrapper(elem) {
 	Clock.call(this, elem);
+
+  this.switchFormat = function() {
+    this.fullFormat = !this.fullFormat;
+  }
+  
+  this.showDate = function () {
+    let date = new Date();
+    this.hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(); 
+    this.min = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();  
+    this.sec = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    let formatDate = this.fullFormat ? this.hours + ':' + this.min + ':' + this.sec : this.hours + ':' + this.min;  
+    this.createDate.innerHTML = `<p>${formatDate}</p>`;
+  };
 }
 
-Full.prototype = Object.create(Clock.prototype);
+ClockWrapper.prototype = Object.create(Clock.prototype);
 
-Full.prototype.showDate = function () {
-  let date = new Date();
-  let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(); 
-  let min = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();  
-  let sec = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-  let formatDate = hours + ':' + min + ':' + sec;  
-	this.createDate.innerHTML = `<p>${formatDate}</p>`;
-};
+let clockWrapper = new ClockWrapper(createDate);
 
-function Short(elem){
-	Clock.call(this, elem);
-}
-
-Short.prototype = Object.create(Clock.prototype);
-
-Short.prototype.showDate = function () {
-  let date = new Date();
-  let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();  
-  let min = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
-  let formatDate = hours + ':' + min;  
-	this.createDate.innerHTML = `<p>${formatDate}</p>`;
-};
 
 createDate.addEventListener('click', (e) => {
   if(e.target.tagName === 'P') {
-    isTrue = !isTrue;
+    clockWrapper.switchFormat();
   }
-})
-
-let full = new Full(createDate);
-let short = new Short(createDate);
+});
 
 setInterval(() => {
-  isTrue === true ? full.showDate() : short.showDate() 
+  clockWrapper.showDate() 
 }, 500);
 
 
